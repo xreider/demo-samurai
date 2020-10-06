312e1ba891e6bab6aa2c281a41cd570f4b4412a7
unknown <xreider@yandex.ru>
2020-10-06 16:59:27 +1000

    Updates

---
+++ precache-manifest.d727dd3b8376e0f8f827043dcc052e6f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e368dc2e67e217692a85eb9466d827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da3eb490c974276e8df2",
+    "url": "/static/css/4.c8c46aa4.chunk.css"
+  },
+  {
+    "revision": "a6b21ecd79f14c9b15de",
+    "url": "/static/css/5.86acde9f.chunk.css"
+  },
+  {
+    "revision": "7d724c3b9f5faf60493f",
+    "url": "/static/css/6.9bbf7e43.chunk.css"
+  },
+  {
+    "revision": "73263367be7cc60f1259",
+    "url": "/static/css/7.de7ddd83.chunk.css"
+  },
+  {
+    "revision": "0cb4c42aaa0f0f204d02",
+    "url": "/static/css/main.6634ba1d.chunk.css"
+  },
+  {
+    "revision": "a3bc8fe02208f5b54e96",
+    "url": "/static/js/0.ccf4fa7d.chunk.js"
+  },
+  {
+    "revision": "e5eea7ea0c25c6f2ff9f",
+    "url": "/static/js/3.f345c65b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/3.f345c65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3eb490c974276e8df2",
+    "url": "/static/js/4.e47d13e5.chunk.js"
+  },
+  {
+    "revision": "a6b21ecd79f14c9b15de",
+    "url": "/static/js/5.84d1b109.chunk.js"
+  },
+  {
+    "revision": "7d724c3b9f5faf60493f",
+    "url": "/static/js/6.b09054a3.chunk.js"
+  },
+  {
+    "revision": "73263367be7cc60f1259",
+    "url": "/static/js/7.4db7c6ea.chunk.js"
+  },
+  {
+    "revision": "0cb4c42aaa0f0f204d02",
+    "url": "/static/js/main.31356b73.chunk.js"
+  },
+  {
+    "revision": "9e495bc21cf31a1ab775",
+    "url": "/static/js/runtime-main.c78778e7.js"
+  },
+  {
+    "revision": "73dd65433ec58220f226fae9fa73b567",
+    "url": "/static/media/logo.73dd6543.png"
+  },
+  {
+    "revision": "b4c52705cd682787c83b6e0e9e1ae9ab",
+    "url": "/static/media/spinner.b4c52705.svg"
+  },
+  {
+    "revision": "489bc18cc7f9ff83fd1f8f8a7a3de28c",
+    "url": "/static/media/spinnerBlue.489bc18c.svg"
+  }
+]);
